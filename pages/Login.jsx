cc3b49dc2e4ff0ab65dfc83e50544c628e363b52
Sergio Lopez Rey <sergiolopezrey@hotmail.com>
2022-04-22 15:59:15 -0300

    init

---
+++ pages/Login.jsx
@@ -0,0 +1,11 @@
+const Login = () => {
+  return (
+    <>
+      <h1>Inicia sesion</h1>
+      <input type="text" name="" id="" />
+      <input type="text" name="" id="" />
+    </>
+  );
+};
+
+export default Login;
